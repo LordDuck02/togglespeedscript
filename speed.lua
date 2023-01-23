40bafa2d56dc355458a56426328e2f63b5a5a2bb
LordDuck02 <97839276+LordDuck02@users.noreply.github.com>
2023-01-23 13:33:50 -0300

    Add files via upload

---
+++ speed.lua
@@ -0,0 +1,18 @@
+local plr = game:GetService('Players').LocalPlayer
+local UIS = game:GetService('UserInputService')
+local speeding = false
+local speed = 40
+local default = 16
+
+
+UIS.InputBegan:Connect(function(Input)
+if Input.KeyCode == Enum.KeyCode.Z then
+if speeding == false then
+speeding = true
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = speed
+else
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = default
+speeding = false
+end
+end
+end)
